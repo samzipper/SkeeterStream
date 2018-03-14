dfe95d09776ea406cdf3d7eb1bb32035bde64e5a
Sam <samuelczipper@gmail.com>
2018-03-14 10:16:21 -0700

    Add file to set up Twitter API token

---
+++ rtweet_SetUpToken.R
@@ -0,0 +1,45 @@
+## rtweet_SetUpToken.R
+#' This script is intended to set up the token necessary to access the
+#' Twitter API, so it does not need to be completed each time.
+#' 
+#' Tutorial: http://rtweet.info/articles/auth.html
+
+rm(list=ls())
+
+require(rtweet)
+
+## first: create app, get API key and secret from apps.twitter.com
+
+## whatever name you assigned to your created app
+appname <- "SkeeterStream"
+
+## api key
+key <- "yrqbPnvreu8coSw6mkZ69Yoaj"
+
+## api secret
+secret <- "uS6PuCiEH3Gjz7PVmaMkWAX3wai4wAr0iAKDGwGO8Mn4fAeClT"
+
+## create token named "twitter_token"
+twitter_token <- create_token(
+  app = appname,
+  consumer_key = key,
+  consumer_secret = secret)
+
+## path of home directory
+home_directory <- path.expand("~/")
+
+## combine with name for token
+file_name <- file.path(home_directory, "twitter_token.Rds")
+
+## save token to home directory
+saveRDS(twitter_token, file = file_name)
+
+## On my mac, the .Renviron text looks like this:
+##     TWITTER_PAT=/Users/mwk/twitter_token.rds
+
+## assuming you followed the procodures to create "file_name"
+##     from the previous code chunk, then the code below should
+##     create and save your environment variable.
+cat(paste0("TWITTER_PAT=", file_name),
+    file = file.path(home_directory, ".Renviron"),
+    append = TRUE)
